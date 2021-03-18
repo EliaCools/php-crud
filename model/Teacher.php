c7866a20522b850522e265abb9fcf61949d24bb0
Paul Sempels <paulsempels@gmail.com>
2021-03-18 10:53:06 +0100

    preparing teacher entity for implementation

--- model/Teacher.php
+++ model/Teacher.php
@@ -1,7 +1,20 @@
 <?php
 
 
-class Teacher extends Person
+class Teacher
 {
+    private int $ID;
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+    private int $phone;
 
+    public function __construct($ID,$fName,$lName,$email,$phone){
+        $this->ID = $ID;
+        $this->firstName = $fName;
+        $this->lastName = $lName;
+        $this->email = $email;
+        $this->phone = $phone;
+
+    }
 }
