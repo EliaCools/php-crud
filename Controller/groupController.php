6b8dd92b0c6a962fd33290f4a2ddeaa73acfc5e0
Paul Sempels <paulsempels@gmail.com>
2021-03-18 21:07:05 +0100

    progress on grouploader

--- Controller/groupController.php
+++ Controller/groupController.php
@@ -6,8 +6,6 @@ class groupController
     public function render(array $GET, array $POST)
     {
 
-
-
         if ($_GET["page"] == "group" && $_GET["action"] == "overview") {
             require 'view/groupOverview.php';
         }
--- model/GroupLoader.php
+++ model/GroupLoader.php
@@ -1,25 +1,50 @@
 <?php
 
-
 class GroupLoader
 {
-    public function fetchGroup()
+    public function insertGroup(Group $group): void
     {
         $pdo = openConnection();
 
-        $sql = 'SELECT * FROM class';
+        $sql = 'INSERT INTO crud.class (className,classLocation,subject)
+                VALUES (:className, :classLocation, :subject)';
         $handle = $pdo->prepare($sql);
-        return $handle->execute();
+        $handle->bindvalue(':className', $group->getName());
+        $handle->bindvalue(':classLocation', $group->getLocation());
+        $handle->bindvalue(':subject', $group->getSubject());
+        $handle->execute();
     }
 
-//$selectedUser['sports'] = [];
-//$handle = $pdo->prepare('SELECT sport FROM sport where user_id = :id GROUP BY user_id');
-//$handle->bindValue(':id', $_GET['id']);
-//
-//foreach ($handle->fetchAll() as $sport) {
-//$selectedUser['sports'][] = $sport['sport'];
-//
-//
-//}
-//$handle->execute();
+    public function fetchAllGroups() : array
+    {
+        $pdo = openConnection();
+
+        $sql = 'SELECT className, subject, classLocation FROM class';
+        $handle = $pdo->prepare($sql);
+        $handle->execute();
+        return $handle->fetchAll();
+    }
+
+    public function fetchDetailed() : array
+    {
+        $pdo = openConnection();
+        $sql = 'SELECT * from class';
+        $handle = $pdo->prepare($sql);
+
+
+    }
+
+    public function updateGroup(Group $group) : array
+    {
+        $pdo = openConnection();
+        $sql = 'UPDATE class SET className = :className ,classLocation = :location, subject = :subject 
+        WHERE classID= :ID';
+        $handle = $pdo->prepare($sql);
+        $handle->bindValue(':className',$group->getName());
+        $handle->bindValue(':location',$group->getLocation());
+        $handle->bindValue(':subject',$group->getSubject());
+        $handle->bindValue(':ID',$group->getID());
+        $handle->execute();
+    }
+
 }
